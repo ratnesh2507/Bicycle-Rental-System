7e48160abb2bf1b471bc5e3aea525cf2b775fd5b
Ratnesh <ratneshbvk2504@gmail.com>
2024-04-27 23:02:40 +0530

    Converting to ES Module

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const authRoutes = require('./routes/authRoutes');
-const bikeRoutes = require('./routes/bikeRoutes');
-const path = require('path');
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const authRoutes = require('./routes/authRoutes');
+// const bikeRoutes = require('./routes/bikeRoutes');
+// const path = require('path');
+import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import authRoutes from './routes/authRoutes.js';
+import bikeRoutes from './routes/bikeRoutes.js';
+import path from 'path';
 const app = express();
 
 // Middleware
@@ -19,15 +26,15 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(publicDirectoryPath, 'index.html'));
 })
 // Connect to MongoDB
-// mongoose.connect('mongodb://localhost:27017/bicycle_renting_app', { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => {
-//         console.log('Connected to database');
-//         // Start server
-//         app.listen(3000, () => {
-//             console.log('Server started on port 3000');
-//         });
-//     })
-//     .catch(err => console.error(err));
-app.listen(3000,()=>{
-    console.log(`Server at 3000`);
-});
+ mongoose.connect('mongodb://localhost:27017/bicycle_renting_app', { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => {
+         console.log('Connected to database');
+         // Start server
+         app.listen(3000, () => {
+             console.log('Server started on port 3000');
+         });
+     })
+     .catch(err => console.error(err));
+// app.listen(3000,()=>{
+//     console.log(`Server at 3000`);
+// });
